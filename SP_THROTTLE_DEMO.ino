cdfa0318f450cfb1d65586707c0a8f81503e205a
glydrfreak <braedinbutler@gmail.com>
2020-02-13 13:23:45 -0700

    Add files via upload
    To do - checksum

--- SP_THROTTLE_DEMO.ino
+++ SP_THROTTLE_DEMO.ino
@@ -1,5 +1,6 @@
 #include <ResponsiveAnalogRead.h>  // smoothing for throttle
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+//#include <Adafruit_ZeroDMA.h>
 #include <Servo.h> // to control ESCs
 #include <Adafruit_BMP3XX.h>
 #include <Adafruit_DRV2605.h>
@@ -14,13 +15,19 @@
 #define TFT_LITE A1
 #define ESC_PIN 12
 
-#define ESC_BAUD_RATE   256000
-#define ESC_DATA_SIZE   20
-#define READ_INTERVAL    0
-#define ESC_TIMEOUT     10
-#define DISPLAY_INTERVAL    100
-#define SEALEVELPRESSURE_HPA 1013.25
-#define ENABLE_BUZ true
+#define BLACK                 ST77XX_BLACK
+#define WHITE                 ST77XX_WHITE
+#define GREEN                 ST77XX_GREEN
+#define YELLOW                ST77XX_YELLOW
+#define RED                   ST77XX_RED
+#define DIGIT_ARRAY_SIZE      7
+#define ESC_BAUD_RATE         256000
+#define ESC_DATA_SIZE         20
+#define READ_INTERVAL         0
+#define ESC_TIMEOUT           10
+#define DISPLAY_INTERVAL      100
+#define SEALEVELPRESSURE_HPA  1013.25
+#define ENABLE_BUZ            false
 
 byte escData[ESC_DATA_SIZE];
 byte prevData[ESC_DATA_SIZE];
@@ -32,6 +39,8 @@ bool armed = false;
 unsigned long displayMillis = 0;
 int potLvl = 0;
 int throttlePWM = 0;
+float throttlePercent = 0;
+float prevThrotPercent = 0;
 bool buttonState = 1;
 bool prevButtonState = 1;
 byte numberPowerCycles = 0;
@@ -45,16 +54,25 @@ uint16_t _outPWM = 0;
 
 //ESC Telemetry
 float volts = 0;
+float prevVolts = 0;
 float temperatureC = 0;
 float amps = 0;
+float prevAmps = 0;
+float kilowatts = 0;
+float prevKilowatts = 0;
 float eRPM = 0;
 float inPWM = 0;
 float outPWM = 0;
 
 //ALTIMETER
 float ambientTempC = 0;
+float prevAmbTempC = 0;
+float ambientTempF = 0;
+float prevAmbTempF = 0;
 float pressureHpa = 0;
 float altitudeM = 0;
+float altitudeFt = 0;
+float prevAltiFt = 0;
 
 ResponsiveAnalogRead pot(POT_PIN, false);
 Servo esc; // Creating a servo class with name of esc
@@ -74,31 +92,11 @@ void setup() {
   pot.setAnalogResolution(4096);
   esc.attach(ESC_PIN);
   esc.writeMicroseconds(0); // make sure motors off
-  Serial1.begin(ESC_BAUD_RATE); 
-  Serial1.setTimeout(ESC_TIMEOUT);
-  pinMode(BUZ_PIN, OUTPUT);
-  if(ENABLE_BUZ){
-    tone(BUZ_PIN, 500);
-    delay(200);
-    tone(BUZ_PIN, 700);
-    delay(200);
-    tone(BUZ_PIN, 900);
-    delay(200);
-    noTone(BUZ_PIN);
-  }
-  tft.initR(INITR_BLACKTAB);          // Init ST7735S chip, black tab
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setCursor(0,0);
-  tft.setTextSize(1);
-  tft.setTextWrap(true);
-  tft.setRotation(1);
-  pinMode(TFT_LITE, OUTPUT);
-  digitalWrite(TFT_LITE, HIGH);  // Backlight on
-  bmp.begin();
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  Serial5.begin(ESC_BAUD_RATE); 
+  Serial5.setTimeout(ESC_TIMEOUT);
+  buzzInit(ENABLE_BUZ);
+  tftInit();
+  bmpInit();
   drv.begin();
   drv.selectLibrary(1);
   drv.setMode(DRV2605_MODE_INTTRIG);
@@ -112,7 +110,7 @@ void setup() {
 
 void loop() {
 
-  handleArming();
+  handleArming(BLACK, WHITE);
   if (armed){ 
     handleThrottle(); 
   }
@@ -120,14 +118,13 @@ void loop() {
   if(millis()-readMillis>=READ_INTERVAL){
     readMillis = millis();
     prepareSerialRead();
-    Serial1.readBytes(escData, ESC_DATA_SIZE);
+    Serial5.readBytes(escData, ESC_DATA_SIZE);
+    //enforceChecksum();  //TODO
+    printRawSentence();
+    parseData(); 
+    readBMP();
   }
-  //enforceChecksum();  //TODO
-  printRawSentence();
-  parseData();
-
-  readBMP();
-
+  
   if(volts<43){
     //vibrateAlert();
   }
@@ -158,15 +155,21 @@ void vibrateNotify(){
 void readBMP(){
   bmp.performReading();
   ambientTempC = bmp.temperature;
+  ambientTempF = ambientTempC*(9/5.0)+32;
   pressureHpa = bmp.pressure;
   altitudeM = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  altitudeFt = (int)(altitudeM*3.28);
 }
 
 
 void handleThrottle() {
   pot.update();
   potLvl = pot.getValue();
-  throttlePWM = map(potLvl, 0, 4095, 1110, 2000); // mapping val to minimum and maximum
+  throttlePWM = mapf(potLvl, 0, 4095, 1110, 2000); // mapping val to minimum and maximum
+  throttlePercent = mapf(throttlePWM, 1112,2000, 0,100);
+  if(throttlePercent<0){
+    throttlePercent = 0;
+  }
   esc.writeMicroseconds(throttlePWM); // using val as the signal to esc
   Serial.print(F("WRITING: "));
   Serial.println(throttlePWM);
@@ -174,64 +177,39 @@ void handleThrottle() {
 
 
 void updateDisplay(){
-  //dispPot(20,10,2);
-  dispVolts(20,10,2);
-  dispAmps(104,10,2);
-  dispThrottle(20,40,2);
-  dispAlti(20,70,2);
-  dispAmbTemp(20,100,2);
-  dispPowerCycles(104,100,2);
-}
+  dispValue(volts, prevVolts, 4, 1, 96, 42, 2, BLACK, WHITE);
+  tft.print("V");
+  
+  dispValue(amps, prevAmps, 3, 0, 108, 70, 2, BLACK, WHITE);
+  tft.print("A");
 
+  dispValue(kilowatts, prevKilowatts, 4, 1, 10, 55, 2, BLACK, WHITE);
+  tft.print("kW");
 
-void dispVolts(int x, int y, int textSize){
-  int maxDigits = 5;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK); //change these to black when finished designing the layout of the display
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  tft.print(volts,1);
-  tft.print(F("V"));
-}
-
-
-void dispAmps(int x, int y, int textSize){
-  int maxDigits = 4;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  tft.print(amps,0);
-  tft.print(F("A"));
-}
-
-
-void dispThrottle(int x, int y, int textSize){
-  int maxDigits = 4;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  int throttlePercent = map(throttlePWM, 1112,2000, 0,100);
-  if(throttlePercent<0){
-    throttlePercent = 0;
+  if(throttlePercent>66){
+    tft.fillRect(0, 0, map(throttlePercent, 0,100, 0,108), 36, GREEN);
   }
-  tft.print(throttlePercent);
-  tft.print(F("%"));
-}
+  else if(throttlePercent>33){
+    tft.fillRect(0, 0, map(throttlePercent, 0,100, 0,108), 36, YELLOW);
+  }
+  else{
+    tft.fillRect(0, 0, map(throttlePercent, 0,100, 0,108), 36, RED);
+  }
+  tft.fillRect(map(throttlePercent, 0,100, 0,108), 0, map(throttlePercent, 0,100, 108,0), 36, WHITE);
+  dispValue(throttlePercent, prevThrotPercent, 3, 0, 108/*55*/, 10, 2, BLACK, WHITE);
+  tft.print("%");
 
-
-void dispPot(int x, int y, int textSize){
-  int maxDigits = 4;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  tft.print(potLvl);
+  tft.fillRect(0, 36, 160, 1, BLACK);
+  tft.fillRect(108, 0, 1, 36, BLACK);
+  tft.fillRect(0, 92, 160, 1, BLACK);
+  
+  dispValue(altitudeFt, prevAltiFt, 5, 0, 70, 100, 2, BLACK, WHITE);
+  tft.print("ft");
+  
+  dispValue(ambientTempF, prevAmbTempF, 3, 0, 10, 100, 2, BLACK, WHITE);
+  tft.print("F");
+  
+  //dispPowerCycles(104,100,2);
 }
 
 
@@ -246,46 +224,74 @@ void dispPowerCycles(int x, int y, int textSize){
 }
 
 
-void dispBtn(int x, int y, int textSize){
-  int maxDigits = 1;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
+//**************************************************************************************//
+//  Helper function to print values without flashing numbers due to slow screen refresh.
+//  This function only re-draws the digit that needs to be updated.
+//**************************************************************************************//
+void dispValue(float &value, float &prevVal, int maxDigits, int precision, int x, int y, int textSize, int textColor, int background){
+  int numDigits = 0;
+  char prevDigit[DIGIT_ARRAY_SIZE] = {};
+  char digit[DIGIT_ARRAY_SIZE] = {};
+  String prevValTxt = String(prevVal, precision);
+  String valTxt = String(value, precision);
+  prevValTxt.toCharArray(prevDigit, maxDigits+1);
+  valTxt.toCharArray(digit, maxDigits+1);
+
+  // COUNT THE NUMBER OF DIGITS THAT NEED TO BE PRINTED:
+  for(int i=0; i<maxDigits; i++){
+    if(digit[i]){
+      numDigits++;
+    }
+  }
+  
+  //tft.fillScreen(ST77XX_WHITE);  // before
+  
   tft.setTextSize(textSize);
-  tft.print(buttonState);
+  tft.setCursor(x,y);
+  
+  // PRINT LEADING SPACES TO RIGHT-ALIGN:
+  for(int i=0; i<(maxDigits-numDigits); i++){
+    tft.setTextColor(background);
+    tft.print(char(218));
+    tft.setTextColor(textColor);
+  }
+  
+  // ERASE ONLY THE NESSESARY DIGITS:
+  for(int i=0; i<numDigits; i++){
+    if(digit[i]!=prevDigit[i]){
+      tft.setTextColor(background);
+      tft.print(char(218));
+      tft.setTextColor(textColor);
+    }
+    else{
+      tft.print(digit[i]);
+    }
+  }
+  
+  // BACK TO THE BEGINNING:
+  tft.setCursor(x,y);
+
+  // ADVANCE THE CURSOR TO THE PROPER LOCATION:
+  for(int i=0; i<(maxDigits-numDigits); i++){
+    tft.setTextColor(background);
+    tft.print(char(218));
+    tft.setTextColor(textColor);
+  }
+
+  // PRINT THE DIGITS THAT NEED UPDATING
+  for(int i=0; i<numDigits; i++){
+    tft.print(digit[i]);
+  }
+
+  prevVal = value;
 }
 
 
-void dispAlti(int x, int y, int textSize){
-  int maxDigits = 7;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  tft.print(altitudeM*3.28,0);
-  tft.print(F("ft"));
-}
-
-
-void dispAmbTemp(int x, int y, int textSize){
-  int maxDigits = 4;
-  int width = 6*textSize;
-  int height = 8*textSize;
-  tft.fillRect(x, y, width*maxDigits, height, ST77XX_BLACK);
-  tft.setCursor(x,y);
-  tft.setTextSize(textSize);
-  tft.print(ambientTempC*(9/5.0)+32,0);
-  tft.print(F("F"));
-}
-
-
-void handleArming(){
+void handleArming(int textColor, int background){
   if(digitalRead(BTN_PIN)==LOW){
     bool armReady = false;
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setTextColor(ST77XX_WHITE);
+    tft.fillScreen(background);
+    tft.setTextColor(textColor);
     tft.setCursor(0,0);
     if(!armed){tft.print(F("ARMING IN: "));}
     else{tft.print(F("DISARMING IN: "));}
@@ -297,8 +303,12 @@ void handleArming(){
       if(armingIn<1){
         armReady = true;
       }
+      if(armReady || digitalRead(BTN_PIN)==HIGH){
+        tft.fillScreen(background);
+        //dispAltiPortion(20,70,2);
+      }
     }
-    tft.fillScreen(ST77XX_BLACK);
+    //tft.fillScreen(ST77XX_BLACK);
     unsigned long postPressMillis = millis();
     if(postPressMillis-prePressMillis>3000){
       if(!armed){
@@ -354,9 +364,44 @@ void disarmSystem() {
 }
 
 
+void bmpInit(){
+  bmp.begin();
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+}
+
+
+void buzzInit(bool enableBuz){
+  pinMode(BUZ_PIN, OUTPUT);
+  if(enableBuz){
+    tone(BUZ_PIN, 500);
+    delay(200);
+    tone(BUZ_PIN, 700);
+    delay(200);
+    tone(BUZ_PIN, 900);
+    delay(200);
+    noTone(BUZ_PIN);
+  }
+}
+
+
+void tftInit(){
+  tft.initR(INITR_BLACKTAB);          // Init ST7735S chip, black tab
+  tft.fillScreen(WHITE);
+  tft.setTextColor(BLACK);
+  tft.setCursor(0,0);
+  tft.setTextSize(1);
+  tft.setTextWrap(true);
+  tft.setRotation(1);
+  pinMode(TFT_LITE, OUTPUT);
+  digitalWrite(TFT_LITE, HIGH);  // Backlight on
+}
+
+
 void prepareSerialRead(){  
-  while(Serial1.available()>0){
-    byte t = Serial1.read();
+  while(Serial5.available()>0){
+    byte t = Serial5.read();
   }
 }
 
@@ -402,8 +447,8 @@ void printRawSentence(){
 void parseData(){
   // LSB First
   
-  //_volts = word(escData[1], escData[0]);
-  _volts = ((unsigned int)escData[1] << 8) + escData[0];
+  _volts = word(escData[1], escData[0]);
+  //_volts = ((unsigned int)escData[1] << 8) + escData[0];
   volts = _volts/100.0;
   //reading 23.00 = 22.7 actual
   //reading 16.00 = 15.17 actual
@@ -420,6 +465,8 @@ void parseData(){
   amps = _amps/10.0;
   Serial.print("Amps: ");
   Serial.println(amps);
+
+  kilowatts = amps*volts/1000.0;
 
   // 7 and 6 are reserved bytes
   
@@ -447,4 +494,9 @@ void parseData(){
   // 17 and 16 are reserved bytes
   // 19 and 18 is checksum
   
+}
+
+
+double mapf(double x, double in_min, double in_max, double out_min, double out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
