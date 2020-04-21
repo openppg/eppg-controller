deee8fdaf7ccc09fda923290e7fb15a3f94620cc
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2020-04-20 22:14:34 -0400

    Formatting and watchdog

--- eppg-controller.ino
+++ eppg-controller.ino
@@ -33,40 +33,40 @@ using namespace Adafruit_LittleFS_Namespace;
 const uint8_t SymbolBitmaps[] PROGMEM = {
 // Power symbol
 /*| 8 4 2 1 8 4 2 1 8 4 2 1 8 4 2 1 |*/
-/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00, 
-/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00, 
-/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00, 
-/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40, 
+/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00,
+/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00,
+/*| . . . . . . . X . . . . . . . . |*/  0x01,0x00,
+/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40,
 /*| . . . X X . . X . . X X . . . . |*/  0x19,0x30,
-/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08, 
-/*| . X . . . . . X . . . . . X . . |*/  0x41,0x04, 
-/*| X . . . . . . X . . . . . . X . |*/  0x81,0x02, 
-/*| X . . . . . . X . . . . . . X . |*/  0x81,0x02, 
+/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08,
+/*| . X . . . . . X . . . . . X . . |*/  0x41,0x04,
 /*| X . . . . . . X . . . . . . X . |*/  0x81,0x02,
-/*| X . . . . . . , . . . . . . X . |*/  0x80,0x02, 
-/*| X . . . . . . , . . . . . . X . |*/  0x80,0x02, 
-/*| . X . . . . . , . . . . . X . . |*/  0x40,0x04, 
-/*| . . X . . . . , . . . . X . . . |*/  0x20,0x08, 
+/*| X . . . . . . X . . . . . . X . |*/  0x81,0x02,
+/*| X . . . . . . X . . . . . . X . |*/  0x81,0x02,
+/*| X . . . . . . , . . . . . . X . |*/  0x80,0x02,
+/*| X . . . . . . , . . . . . . X . |*/  0x80,0x02,
+/*| . X . . . . . , . . . . . X . . |*/  0x40,0x04,
+/*| . . X . . . . , . . . . X . . . |*/  0x20,0x08,
 /*| . . . X X . . , . . X X . . . . |*/  0x18,0x30,
 /*| . . . . . X X X X X . . . . . . |*/  0x07,0xc0,
 // Bluetooth
 /*| 8 4 2 1 8 4 2 1 8 4 2 1 8 4 2 1 |*/
-/*| . . . . . . . X X . . . . . . . |*/  0x01,0x80, 
-/*| . . . . . . . X . X . . . . . . |*/  0x01,0x40, 
-/*| . . . . . . . X . . X . . . . . |*/  0x01,0x20, 
-/*| . . . . . . . X . . . X . . . . |*/  0x01,0x10, 
-/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08,
-/*| . . . X . . . X . . . X . . . . |*/  0x11,0x10, 
-/*| . . . . X . . X . . X . . . . . |*/  0x09,0x20, 
-/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40, 
-/*| . . . . . . X X X . . . . . . . |*/  0x03,0x80, 
-/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40,
-/*| . . . . X . . X . . X . . . . . |*/  0x09,0x20, 
-/*| . . . X . . . X . . . X . . . . |*/  0x11,0x10, 
-/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08, 
-/*| . . . . . . . X . . . X . . . . |*/  0x01,0x10, 
+/*| . . . . . . . X X . . . . . . . |*/  0x01,0x80,
+/*| . . . . . . . X . X . . . . . . |*/  0x01,0x40,
 /*| . . . . . . . X . . X . . . . . |*/  0x01,0x20,
-/*| . . . . . . . X . X . . . . . . |*/  0x01,0x40, 
+/*| . . . . . . . X . . . X . . . . |*/  0x01,0x10,
+/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08,
+/*| . . . X . . . X . . . X . . . . |*/  0x11,0x10,
+/*| . . . . X . . X . . X . . . . . |*/  0x09,0x20,
+/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40,
+/*| . . . . . . X X X . . . . . . . |*/  0x03,0x80,
+/*| . . . . . X . X . X . . . . . . |*/  0x05,0x40,
+/*| . . . . X . . X . . X . . . . . |*/  0x09,0x20,
+/*| . . . X . . . X . . . X . . . . |*/  0x11,0x10,
+/*| . . X . . . . X . . . . X . . . |*/  0x21,0x08,
+/*| . . . . . . . X . . . X . . . . |*/  0x01,0x10,
+/*| . . . . . . . X . . X . . . . . |*/  0x01,0x20,
+/*| . . . . . . . X . X . . . . . . |*/  0x01,0x40,
 /*| . . . . . . . X X . . . . . . . |*/  0x01,0x80,
 // Bluetooth mini
 /*| 8 4 2 1 8 4 2 1 |*/
@@ -182,7 +182,6 @@ void setup() {
   refreshDeviceData();
 
   setupBluetooth();
-  
 }
 
 void setupBluetooth() {
@@ -279,6 +278,7 @@ void loop() {
   // From Serial to both Serial & webUSB
   if (Serial.available())  echo_all(Serial.read());
   threads.run();
+  Watchdog.reset();
 }
 
 void checkButtons() {
@@ -767,14 +767,14 @@ void cent_bleuart_rx_callback(BLEClientUart& cent_uart) {
   }
 }
 
-void drawSymbol(uint16_t x, uint16_t y, uint8_t c, uint16_t color, uint16_t bg, uint8_t char_size){
-  if( (c>=32) && (c<=126) ){ //If it's 33-126 then use standard mono 18 font
+void drawSymbol(uint16_t x, uint16_t y, uint8_t c, uint16_t color, uint16_t bg, uint8_t char_size) {
+  if ( ( c >= 32 ) && ( c <= 126 ) ) { // If it's 33-126 then use standard mono 18 font
       display.setFont();
   } else {
-    display.setFont(&Symbol18);//Otherwise use special symbol font
-    if (c>126) {      //Remap anything above 126 to be in the range 32 and upwards
+    display.setFont(&Symbol18);  // Otherwise use special symbol font
+    if ( c > 126 ) {  // Remap anything above 126 to be in the range 32 and upwards
       c-=(127-32);
     }
   }
-  display.drawChar(x,y,c,color,bg,char_size);
+  display.drawChar(x, y, c, color, bg, char_size);
 }
