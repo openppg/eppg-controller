c09f15caa7d83a9745569161c415ec34ece7fe08
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2020-12-02 22:36:56 -0500

    cleanup

--- ble.ino
+++ ble.ino
@@ -1,28 +1,28 @@
- /* Throttle 2 controller service UUID string: 8877FB19-E00B-40BE-905E-ACB42C39E6B8 */
-uint8_t t2cs_byte_array[16u] = {0x88,0x77,0xFB,0x19,0xE0,0x0B,0x40,0xBE,0x90,0x5E,0xAC,0xB4,0x2C,0x39,0xE6,0xB8}; 
-/* Throttle value char UUID string: 5A57F691-C0B9-45DD-BDF1-279681212C29 */
-uint8_t t2ctc_byte_array[16u] = {0x5A,0x57,0xF6,0x91,0xC0,0xB9,0x45,0xDD,0xBD,0xF1,0x27,0x96,0x81,0x21,0x2C,0x29}; 
+/* Bluetooth config */
 
-BLEService        hrms = BLEService(t2cs_byte_array);
-BLECharacteristic hrmc = BLECharacteristic(t2ctc_byte_array);
-BLECharacteristic bslc = BLECharacteristic(UUID16_CHR_BODY_SENSOR_LOCATION);
+// Throttle 2 controller service UUID string: 8877FB19-E00B-40BE-905E-ACB42C39E6B8
+static uint8_t t2cs_uuid_bytes[16u] = {0x88,0x77,0xFB,0x19,0xE0,0x0B,0x40,0xBE,0x90,0x5E,0xAC,0xB4,0x2C,0x39,0xE6,0xB8};
+// Throttle value char UUID string: 5A57F691-C0B9-45DD-BDF1-279681212C29
+static uint8_t t2ctc_uuid_bytes[16u] = {0x5A,0x57,0xF6,0x91,0xC0,0xB9,0x45,0xDD,0xBD,0xF1,0x27,0x96,0x81,0x21,0x2C,0x29};
+// Armed status UUID string: 28913A56-5701-4B27-85DB-50985F224847
+static uint8_t t2cac_uuid_bytes[16u] = {0x28,0x91,0x3A,0x56,0x57,0x01,0x4B,0x27,0x85,0xDB,0x50,0x98,0x5F,0x22,0x48,0x47};
+
+BLEService        t2cs = BLEService(t2cs_uuid_bytes);
+BLECharacteristic t2ctc = BLECharacteristic(t2ctc_uuid_bytes);
+BLECharacteristic t2cac = BLECharacteristic(t2cac_uuid_bytes);
 
 BLEDfu bledfu;
 
 uint16_t bps = 0;
 
-void setupBle(){
+void setupBle() {
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
 
-  Serial.println("Bluefruit52 HRM Example");
-  Serial.println("-----------------------\n");
+  Serial.println("Setting up Ble");
 
-  // Initialise the Bluefruit module
-  Serial.println("Initialise the Bluefruit nRF52 module");
   Bluefruit.begin();
 
   // Set the advertised device name (keep it short!)
-  Serial.println("Setting Device Name to 'Feather52 HRM'");
   Bluefruit.setName("OpenPPG Controller");
   // To be consistent OTA DFU should be added first if it exists
   bledfu.begin();
@@ -37,115 +37,68 @@ void setupBle(){
   bledis.setModel("Controller V2");
   bledis.begin();
 
-  // Start the BLE Battery Service and set it to 100%
+  // Start the BLE Battery Service and set it to 0 by default
   Serial.println("Configuring the Battery Service");
   blebas.begin();
-  blebas.write(100);
+  blebas.write(100); //TODO set to battery
 
-  // Setup the Heart Rate Monitor service using
+  // Setup the Throttle service using
   // BLEService and BLECharacteristic classes
-  Serial.println("Configuring the Heart Rate Monitor Service");
-  setupHRM();
+  Serial.println("Configuring the Throttle Service");
+  setupThrottleBle();
 
   // Setup the advertising packet(s)
   Serial.println("Setting up the advertising payload(s)");
   startAdv();
 
-  Serial.println("Ready Player One!!!");
-  Serial.println("\nAdvertising");
+  Serial.println("Ready - Advertising");
 }
 
 
-void startAdv(void)
-{
+void startAdv(void) {
   // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
 
   // Include HRM Service UUID
-  Bluefruit.Advertising.addService(hrms);
+  Bluefruit.Advertising.addService(t2cs);
 
   // Include Name
   Bluefruit.Advertising.addName();
-  
+
   /* Start Advertising
    * - Enable auto advertising if disconnected
    * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
    * - Timeout for fast mode is 30 seconds
    * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
+   *
    * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html
    */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
 
-void setupHRM(void)
-{
-  // Configure the Heart Rate Monitor service
-  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml
-  // Supported Characteristics:
-  // Name                         UUID    Requirement Properties
-  // ---------------------------- ------  ----------- ----------
-  // Heart Rate Measurement       0x2A37  Mandatory   Notify
-  // Body Sensor Location         0x2A38  Optional    Read
-  // Heart Rate Control Point     0x2A39  Conditional Write       <-- Not used here
-  hrms.begin();
+void setupThrottleBle(void) {
+  t2cs.begin();
 
   // Note: You must call .begin() on the BLEService before calling .begin() on
   // any characteristic(s) within that service definition.. Calling .begin() on
   // a BLECharacteristic will cause it to be added to the last BLEService that
   // was 'begin()'ed!
 
-  // Configure the Heart Rate Measurement characteristic
-  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
-  // Properties = Notify
-  // Min Len    = 1
-  // Max Len    = 8
-  //    B0      = UINT8  - Flag (MANDATORY)
-  //      b5:7  = Reserved
-  //      b4    = RR-Internal (0 = Not present, 1 = Present)
-  //      b3    = Energy expended status (0 = Not present, 1 = Present)
-  //      b1:2  = Sensor contact status (0+1 = Not supported, 2 = Supported but contact not detected, 3 = Supported and detected)
-  //      b0    = Value format (0 = UINT8, 1 = UINT16)
-  //    B1      = UINT8  - 8-bit heart rate measurement value in BPM
-  //    B2:3    = UINT16 - 16-bit heart rate measurement value in BPM
-  //    B4:5    = UINT16 - Energy expended in joules
-  //    B6:7    = UINT16 - RR Internal (1/1024 second resolution)
-  hrmc.setProperties(CHR_PROPS_NOTIFY);
-  hrmc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  hrmc.setFixedLen(2);
-  hrmc.setCccdWriteCallback(cccd_callback);  // Optionally capture CCCD updates
-  hrmc.begin();
+  t2ctc.setProperties(CHR_PROPS_NOTIFY);
+  t2ctc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  t2ctc.setFixedLen(2);
+  t2ctc.setCccdWriteCallback(cccd_callback);  // Optionally capture CCCD updates
+  t2ctc.begin();
   uint8_t hrmdata[2] = { 0b00000110, 0x40 }; // Set the characteristic to use 8-bit values, with the sensor connected and detected
-  hrmc.write16(5);
-
-  // Configure the Body Sensor Location characteristic
-  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.body_sensor_location.xml
-  // Properties = Read
-  // Min Len    = 1
-  // Max Len    = 1
-  //    B0      = UINT8 - Body Sensor Location
-  //      0     = Other
-  //      1     = Chest
-  //      2     = Wrist
-  //      3     = Finger
-  //      4     = Hand
-  //      5     = Ear Lobe
-  //      6     = Foot
-  //      7:255 = Reserved
-  bslc.setProperties(CHR_PROPS_READ);
-  bslc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  bslc.setFixedLen(1);
-  bslc.begin();
-  bslc.write8(2);    // Set the characteristic to 'Wrist' (2)
+  t2ctc.write16(0);
 }
 
-void connect_callback(uint16_t conn_handle)
-{
+void connect_callback(uint16_t conn_handle) {
   // Get the reference to current connection
   BLEConnection* connection = Bluefruit.Connection(conn_handle);
 
@@ -161,8 +114,7 @@ void connect_callback(uint16_t conn_handle)
  * @param conn_handle connection where this event happens
  * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
  */
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
+void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
   (void) conn_handle;
   (void) reason;
 
@@ -170,17 +122,15 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   Serial.println("Advertising!");
 }
 
-void cccd_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_value)
-{
+void cccd_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_value) {
     // Display the raw request packet
     Serial.print("CCCD Updated: ");
     //Serial.printBuffer(request->data, request->len);
-    Serial.print(cccd_value);
-    Serial.println("");
+    Serial.println(cccd_value);
 
     // Check the characteristic this CCCD update is associated with in case
     // this handler is used for multiple CCCD records.
-    if (chr->uuid == hrmc.uuid) {
+    if (chr->uuid == t2ctc.uuid) {
         if (chr->notifyEnabled(conn_hdl)) {
             Serial.println("Measurement 'Notify' enabled");
         } else {
@@ -189,23 +139,19 @@ void cccd_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_valu
     }
 }
 
-void throttleBleLoop(int throttleVal){
+void throttleBleLoop(int throttleVal) {
   Serial.print("throttle is2 ");
   Serial.println(throttleVal);
   if ( Bluefruit.connected() ) {
-    //uint8_t hrmdata[2] = { 0b00000110, (uint8_t*)throttleVal };           // Sensor connected, increment BPS value
-    //uint8_t hrmdata[2] = { 0b00000110, bps++ };            // Sensor connected, increment BPS value
     // Note: We use .notify instead of .write!
     // If it is connected but CCCD is not enabled
     // The characteristic's value is still updated although notification is not sent
-    if ( hrmc.notify16((uint16_t)throttleVal)){
-      Serial.print("Measurement updated to: "); Serial.println((uint16_t)throttleVal); 
-    }else{
+    if ( t2ctc.notify16((uint16_t)throttleVal) ) {
+      Serial.print("Measurement updated to: "); Serial.println((uint16_t)throttleVal);
+    } else {
       Serial.println("ERROR: Notify not set in the CCCD or not connected!");
     }
-  }
-  else
-  {
-    Serial.println("NC");
+  } else {
+    // Serial.println("NC");
   }
 }
