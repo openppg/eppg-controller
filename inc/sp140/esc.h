525d200cdd9afe9af7b647eca51b818bcd057eb8
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2024-10-23 18:27:28 -0500

    move UART ESC struct and defines

--- inc/sp140/esc.h
+++ inc/sp140/esc.h
@@ -14,11 +14,14 @@
   #include <CanardAdapter.h>
 #endif
 
-// ESC communication parameters
-#define ESC_BAUD_RATE         115200
-#define ESC_DATA_V2_SIZE      22
-#define READ_INTERVAL         0
-#define ESC_TIMEOUT           15
+#ifndef CAN_PIO
+  #define ESC_BAUD_RATE         115200
+  #define ESC_DATA_V2_SIZE      22
+  #define ESC_TIMEOUT           15
+
+  SerialESC.begin(ESC_BAUD_RATE);
+  SerialESC.setTimeout(ESC_TIMEOUT);
+#endif
 
 bool initESC(int escPin);
 void setupESCSerial();
@@ -35,5 +38,48 @@ void dumpESCMessages();  // dumps all messages to USBSerial
 
 // External declaration of telemetry data structure
 extern STR_ESC_TELEMETRY_140 escTelemetryData;
+
+// v2 ESC telemetry raw data structure
+typedef struct  {
+  // Voltage
+  int V_HI;
+  int V_LO;
+
+  // Temperature
+  int T_HI;
+  int T_LO;
+
+  // Current
+  int I_HI;
+  int I_LO;
+
+  // Reserved
+  int R0_HI;
+  int R0_LO;
+
+  // eRPM
+  int RPM0;
+  int RPM1;
+  int RPM2;
+  int RPM3;
+
+  // Input Duty
+  int DUTYIN_HI;
+  int DUTYIN_LO;
+
+  // Motor Duty
+  int MOTORDUTY_HI;
+  int MOTORDUTY_LO;
+
+  // Reserved
+  int R1;
+
+  // Status Flags
+  int statusFlag;
+
+  // checksum
+  int CSUM_HI;
+  int CSUM_LO;
+} telem_esc_t;
 
 #endif  // INC_SP140_ESC_H_
